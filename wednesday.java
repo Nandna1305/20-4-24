a939269c46ba05eaa6c5d56f00781c18a6f08832
unknown <tommyandjerry1330@gmail.com>
2024-04-20 08:27:51 +0530

    added file wednesday.java

---
+++ wednesday.java
@@ -0,0 +1,16 @@
+class wednesday { 
+    public static void main (String args[]) {
+           int num1 = 20;
+           int num2 = 80;
+           if(num1==num2)
+           {
+               System.out.println("numbers are equal");
+           }
+           else
+           {
+               System.out.println("numbers are not equal");
+           }
+       }
+       
+   }
+   
