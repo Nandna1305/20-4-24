651d34a9234385bde459796e4ceb4fd8366e6833
unknown <tommyandjerry1330@gmail.com>
2024-04-20 10:23:16 +0530

    added file february.java

---
+++ february.java
@@ -0,0 +1,13 @@
+public class february {
+    public static void main (String args[]) {
+        int num = 15;
+        if(num%3==0 && num%5==0)
+        {
+            System.out.println("the no is divisible by 3 and 5");
+        }
+        else{
+            System.out.println("the no is not divisible by 3 and 5");
+        }
+    }
+    
+}
