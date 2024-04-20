0401b1f3f6ad08673029125f8382b6f4be0deb1c
unknown <tommyandjerry1330@gmail.com>
2024-04-20 08:07:22 +0530

    added file monday.java

---
+++ monday.java
@@ -0,0 +1,13 @@
+class monday{
+    public static void main(String args[]) {
+        boolean a = false;
+        if(a)
+        {
+            System.out.println("today is a good day");
+        }
+        else
+        {
+            System.out.println("this is a bad day");
+        }
+    }
+}
