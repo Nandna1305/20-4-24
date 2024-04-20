d9fea9f4965ffebf38fa9267db3f1ce3fdc516ca
unknown <tommyandjerry1330@gmail.com>
2024-04-20 09:47:53 +0530

    added file sunday.java

---
+++ sunday.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+class sunday {
+    public static void main(String args[]) {
+        Scanner john = new Scanner(System.in);
+        int income = john.nextInt();
+        if(income>7000)
+        {
+            System.out.println("schlorship is available");
+        }
+        else
+        {
+            System.out.println("schlorship is not available");
+        }
+    }
+}
