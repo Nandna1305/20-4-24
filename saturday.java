2c6f68db7c8731f362117ec39fe7e0fd81d11674
unknown <tommyandjerry1330@gmail.com>
2024-04-20 09:37:37 +0530

    added file saturday.java

---
+++ saturday.java
@@ -0,0 +1,18 @@
+public class saturday 
+{
+    public static void main (String args[]) 
+    {
+        int a = 35;
+        if(a>50)
+        {
+            System.out.println("pass");
+        }
+        else
+        {
+              System.out.println("false");
+        }
+        
+       
+
+    }
+}
