ff68e72c996ba9ded059dd29fddd5fa1e84ee225
unknown <tommyandjerry1330@gmail.com>
2024-04-20 10:07:24 +0530

    added file january.java

---
+++ january.java
@@ -0,0 +1,14 @@
+class january {
+    public static void main(String args[]) {
+        boolean cricket = true;
+        boolean football = false;
+        if(cricket || football)
+        {
+            System.out.println("play");
+        }
+        else
+        {
+            System.out.println("don't play");
+        }
+    }
+}
