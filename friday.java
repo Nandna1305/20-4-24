14eaf79971bd7b085f213b79c064ec55aab0471d
unknown <tommyandjerry1330@gmail.com>
2024-04-20 09:12:45 +0530

    added file friday.java

---
+++ friday.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+class friday 
+{
+    public static void main(String args[]) 
+    {
+        String RCB = "win";
+        if(RCB.equals("win"))
+        {
+        System.out.println("ee sala cup namdhey");
+        }
+        else
+        {
+            System.out.println("cup ila");
+        }
+    }   
+
+        
+}
