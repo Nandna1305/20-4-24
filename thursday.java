a3a939df2c3d127a8700821ff2dc40e52a7b502f
unknown <tommyandjerry1330@gmail.com>
2024-04-20 08:50:18 +0530

    added file thursday.java

---
+++ thursday.java
@@ -0,0 +1,17 @@
+class thursday { 
+    public static void main (String args[]) {
+           String a = "nandana";
+           String b = "sneha";
+           if(a.equals(b))
+           {
+               System.out.println("strings are equal");
+           }
+           else
+           {
+               System.out.println("strings are not equal");
+           }
+       }
+       
+   }
+   
+
