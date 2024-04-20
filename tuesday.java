49712811004bf01f6510e243f50b9aa9bb5325a3
unknown <tommyandjerry1330@gmail.com>
2024-04-20 08:21:36 +0530

    added file tuesday.java

---
+++ tuesday.java
@@ -0,0 +1,15 @@
+ class tuesday { 
+ public static void main (String args[]) {
+        int num1 = 20;
+        int num2 = 80;
+        if(num1>num2)
+        {
+            System.out.println("number 1 is greater");
+        }
+        else
+        {
+            System.out.println("number 2 is greater");
+        }
+    }
+    
+}
